57129d1212658f3f78c29642868cb47dba10bb43
Jalal <mohd.farhan.jalal@intel.com>
2018-04-26 15:15:57 +0800

    add delete update function

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -7,18 +7,41 @@ import {  Observable } from 'rxjs/Observable'
 export class ItemService {
   itemsCollection: AngularFirestoreCollection<Item>;
   items: Observable<Item[]>;
+  itemDoc: AngularFirestoreDocument<Item>;
 
   constructor(private afs: AngularFirestore) { 
     const settings = {timestampsInSnapshots: true };
     afs.app.firestore().settings(settings);
 
-    this.items = this.afs.collection('Items').valueChanges();
-    console.log(this.items);
+    this.itemsCollection = this.afs.collection('Items');
+
+    //this.items = this.afs.collection('Items').valueChanges();
+    this.items = this.itemsCollection.snapshotChanges().map(changes =>{
+      return changes.map(a =>{
+        const data = a.payload.doc.data() as Item;
+        data.id = a.payload.doc.id;
+        return data;
+      })
+    });
+
   }
 
   getItems(){
     return this.items;
   }
 
+  addItem(item : Item){
+    this.itemsCollection.add(item);
+  }
+
+  deleteItem(item: Item){
+      this.itemDoc = this.afs.doc(`Items/${item.id}`);
+      this.itemDoc.delete();
+  }
+
+  updateItem(item: Item){
+    this.itemDoc = this.afs.doc(`Items/${item.id}`);
+    this.itemDoc.update(item);
+  }
 }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDrl12Ca_6u0nJr5mVbfX4ekM08Q_zUoRU',
+    authDomain: 'fs1dev-82d85.firebaseapp.com',
+    databaseURL: 'https://fs1dev-82d85.firebaseio.com',
+    projectId: 'fs1dev-82d85',
+    storageBucket: 'fs1dev-82d85.appspot.com',
+    messagingSenderId: '935527296398'
+  }
 };
